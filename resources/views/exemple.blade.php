0910b9f936b9ec190488032e6c58eb84808c6a9c
Matheus oliveira <matheusoliveiradesouza190503@gmail.com>
2023-12-06 21:55:55 -0300

    coleta de dados pronta

--- resources/views/exemple.blade.php
+++ resources/views/exemple.blade.php
@@ -1,17 +1,22 @@
 <html>
+
 <head>
     <title>Relatório</title>
 </head>
+
 <body>
     <h1>Relatório</h1>
-    
+
     {{-- Exiba os dados aqui --}}
     <div id="dados-container">
         <!-- Os dados serão exibidos aqui -->
     </div>
-
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    
     <script>
+        var caloriesExpendedData = [];
+        var weightData = [];
+        var heartMinutesData = [];
         function carregarDados(url) {
             $.ajax({
                 url: url,
@@ -26,50 +31,74 @@
             });
         }
 
-        function exibirDados(response) {
-        var container = $('#dados-container');
-        container.empty();
+        var stepsCountData = [];
 
-        // Verifique se a resposta contém a propriedade "Data Points"
-        if (response.hasOwnProperty('Data Points')) {
-            var dados = response['Data Points'];
 
-            // Certifique-se de que dados é uma matriz antes de usar forEach
-            if (Array.isArray(dados) && dados.length > 0) {
-                // Vamos assumir que os dados contêm um objeto com a propriedade "fitValue"
-                var fitValue = dados[0]['fitValue'];
+function exibirDados(response) {
+    var container = $('#dados-container');
+    container.empty();
 
-                // Certifique-se de que fitValue é uma matriz antes de usar forEach
-                if (Array.isArray(fitValue) && fitValue.length > 0) {
-                    // Vamos assumir que fitValue contém um objeto com a propriedade "value"
-                    var value = fitValue[0]['value'];
+    // Itera sobre cada tipo de dado (steps_count, calories_expended, weight, heart_minutes)
+    for (var dataType in response) {
+        if (response.hasOwnProperty(dataType)) {
+            var dataPoints = response[dataType];
 
-                    // Certifique-se de que value é definido antes de acessar suas propriedades
-                    if (value) {
-                        container.append('<p>Peso: ' + value['fpVal'] + '</p>');
+            // Verifica se dataPoints é uma matriz e não está vazio
+            if (Array.isArray(dataPoints) && dataPoints.length > 0) {
+                // Itera sobre cada ponto de dados
+                dataPoints.forEach(function (dataPoint) {
+                    // Verifica se as propriedades essenciais estão presentes
+                    if (dataPoint.hasOwnProperty('value') && dataPoint.hasOwnProperty('startTime') && dataPoint.hasOwnProperty('endTime')) {
+                        // Adapte conforme necessário, dependendo da estrutura específica de cada tipo de dado
+                        var value = dataPoint['value'];
+                        var startTime = dataPoint['startTime'];
+                        var endTime = dataPoint['endTime'];
+
+                        // Adiciona os dados ao array apropriado
+                        switch (dataType) {
+                            case 'steps_count':
+                                stepsCountData.push({ value: value, startTime: startTime, endTime: endTime });
+                                break;
+                            case 'calories_expended':
+                                caloriesExpendedData.push({ value: value, startTime: startTime, endTime: endTime });
+                                break;
+                            case 'weight':
+                                weightData.push({ value: value, startTime: startTime, endTime: endTime });
+                                break;
+                            case 'heart_minutes':
+                                heartMinutesData.push({ value: value, startTime: startTime, endTime: endTime });
+                                break;
+                            default:
+                                console.error('Tipo de dado desconhecido:', dataType);
+                        }
+
+                        
                     } else {
-                        console.error('A propriedade "value" é indefinida.', fitValue);
+                        console.error('Ponto de dados inválido para ' + dataType + ':', dataPoint);
                     }
-                } else {
-                    console.error('A propriedade "fitValue" não é uma matriz ou está vazia.', dados);
-                }
+                });
             } else {
-                console.error('Os "Data Points" não são uma matriz ou estão vazios.', response);
+                console.error('Os dados para ' + dataType + ' não são uma matriz ou estão vazios.', response);
             }
-        } else {
-            console.error('A resposta não contém a propriedade "Data Points".', response);
         }
     }
 
+    // Agora você tem os dados armazenados nas variáveis stepsCountData, caloriesExpendedData, weightData e heartMinutesData
+    console.log('Dados de steps_count:', stepsCountData);
+    console.log('Dados de calories_expended:', caloriesExpendedData);
+    console.log('Dados de weight:', weightData);
+    console.log('Dados de heart_minutes:', heartMinutesData);
 
-        // Carregue os dados inicialmente
-        carregarDados('/relatorioPeso');
+}
 
-        // Atualize os dados a cada 5 segundos (ajuste conforme necessário)
-        setInterval(function () {
-            carregarDados('/relatorioPeso');
-        }, 5000);
-        
+
+
+
+    carregarDados("https://v1.nocodeapi.com/gabrielprisco/fit/fLzdQAHmJTPhNYui/aggregatesDatasets?dataTypeName=steps_count,calories_expended,weight,heart_minutes&timePeriod=7days");
+
+        // Atualize os dados a cada 30 segundos (ajuste conforme necessário)
+        //setInterval(carregarDados("https://v1.nocodeapi.com/gabrielprisco/fit/fLzdQAHmJTPhNYui/aggregatesDatasets?dataTypeName=steps_count,calories_expended,weight,heart_minutes&timePeriod=7days"), 30000);
     </script>
 </body>
+
 </html>
